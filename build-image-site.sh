b62329f57ca139d68aa9717539a088728051c49b
Andreas Friestad <afriestad@users.noreply.github.com>
2020-11-04 23:10:56 +0100

    Fix output path for staticfiles in angular app

--- build-image-site.sh
+++ build-image-site.sh
@@ -1,5 +1,5 @@
 git submodule update
 pushd ./website-app
 npm install
-ng build --prod --baseHref="/blarg/image-arrange" --outputPath="../docs/image-arrange"
+ng build --prod --baseHref="/blarg/image-arrange" --deployUrl="https://afriestad.github.io/blarg/image-arrange/" --outputPath="../docs/image-arrange"
 popd
